ae7f9839542bb3bd4571f26262664306ee70bcb9
Simas <simonas@digitouch.lt>
2019-05-15 09:30:15 +0300

    add google maps integration

---
+++ src/components/layouts/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="navbar">
+    <nav class="deep-purple darken-1">
+      <div class="container">
+        <a href="/" class="brand-logo left">Vue-Geo</a>
+        <ul class="right">
+          <li>
+            <a>Signup</a>
+          </li>
+          <li>
+            <a>Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
---
+++ src/views/GMap.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="map">
+    <div class="google-map" id="map"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'GMap',
+  data() {
+    return {
+      lat: 54.703929,
+      lng: 25.276668,
+    };
+  },
+  mounted() {
+    this.renderMap();
+  },
+  methods: {
+    renderMap() {
+      // eslint-disable-next-line
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+          lat: this.lat,
+          lng: this.lng,
+        },
+        zoom: 15,
+        maxZoom: 15,
+        minZoom: 3,
+        streetViewControl: false,
+      });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+#map {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: #fff;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+</style>
